c116fb08ccdb239725466df55479d9d2ce31e3d6
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2020-08-15 15:15:55 -0400

    adding tile grid

--- src/pages/TileCreator/TileCreator.js
+++ src/pages/TileCreator/TileCreator.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '../../logo.svg';
 import '../../App.css';
 import Links from '../../components/Links';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import BrightnessHighIcon from '@material-ui/icons/BrightnessHigh';
+import { Button } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    flexDirection: 'column',
   },
   tileContainer: {
     border: 'solid',
@@ -19,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'row',
     height: 400,
-    marginTop: 100,
+    margin: 25,
     width: 400,
   },
   tileLeft: { 
@@ -32,11 +34,28 @@ const useStyles = makeStyles((theme) => ({
     flex: 1,
   },
   tileGrid: {
+    display: 'flex',
     border: 'solid',
     borderRadius: 5,
     borderWidth: 1,
     flex: 5,
+    flexDirection: 'column',
     padding: 5,
+  },
+  gridRow: {
+    display: 'flex',
+    flex: 1,
+  },
+  gridSquare: {
+    border: 'solid',
+    borderRadius: 5,
+    borderWidth: 1,
+    flex: 1,
+    transition: '0.4s',
+    '&:hover': {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.common.white
+    },
   },
   tileName: {
     flex: 1,
@@ -56,16 +75,76 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+function GridSquare(props) {
+  const classes = useStyles();
+  return (
+    <div className={classes.gridSquare}>
+      <div>{`${props.x},${props.y}`}</div>
+      <div>{props.type}</div>
+    </div>
+  );
+}
+
+function Grid(props) {
+  const classes = useStyles();
+  const {height, width, moves} = props;
+  return (
+    <div className={classes.tileGrid}>
+      {
+        Array(height).fill(null).map((item, y) => (
+          <div key={y} className={classes.gridRow}>
+            {Array(width).fill(null).map((item, x) => {
+              const movesAtXY = moves.filter((move) => move.x === x && move.y === y);
+              const type = movesAtXY.length ? movesAtXY[0].type : null
+              return <GridSquare key={x} x={x} y={y} type={type}/>
+            })}
+          </div>
+        ))
+      }
+    </div>
+  );
+}
+
 function TileCreator() {
   const classes = useStyles();
-
+  const [side, setSide] = useState(0);
+  const flipTile = () => {
+    setSide(1 - side)
+  }
+  
+  const tileData = {
+    height: 5,
+    width: 5,
+    sides: [
+      // side 1
+      {
+        moves: [
+          {
+            x: 2,
+            y: 1,
+            type: 'm'
+          }
+        ],
+      },
+      // side 2
+      {
+        moves: [
+          {
+            x: 2,
+            y: 3,
+            type: 'm'
+          }
+        ],
+      },
+    ],
+  }
   return (
     <div className={classes.root}>
+      <h1>Tile Creator</h1>
       <div className={classes.tileContainer}>
         <div className={classes.tileLeft}>
-          <div className={classes.tileGrid}>
-            
-          </div>
+          {/* <Grid height={tileData.height} width={tileData.width}/> */}
+          <Grid moves={tileData.sides[side].moves} {...tileData}/>
           <div className={classes.tileName}>
             <Typography className={classes.title}>
               Footman
@@ -78,6 +157,13 @@ function TileCreator() {
           </div>
         </div>
       </div>
+      <Button 
+        variant="contained" 
+        color='primary' 
+        onClick={flipTile}
+      >
+        Flip to side {1 - side + 1}
+      </Button>
     </div>
   );
 }
