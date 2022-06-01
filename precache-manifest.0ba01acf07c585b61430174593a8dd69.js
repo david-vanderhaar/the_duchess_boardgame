2931b8107e99589f3a8c6515eae89c9c1c12d77b
David Vanderhaar <d.vanderhaarhunter@gmail.com>
2022-06-01 17:11:28 -0400

    Updates

---
+++ precache-manifest.0ba01acf07c585b61430174593a8dd69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb35e994d14e4def83f88c082a2232ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67a99cfe8d2e4edaa68f",
+    "url": "./static/css/main.6165e202.chunk.css"
+  },
+  {
+    "revision": "f6524e8b023b2482ab57",
+    "url": "./static/js/2.6f41d706.chunk.js"
+  },
+  {
+    "revision": "20b5e334979bc76b85fbbc790c181326",
+    "url": "./static/js/2.6f41d706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a99cfe8d2e4edaa68f",
+    "url": "./static/js/main.1a409f27.chunk.js"
+  },
+  {
+    "revision": "957b079360f4ae2eefe7",
+    "url": "./static/js/runtime-main.bb11bce3.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  }
+]);
